1f42cc67bc4a1a88211909cee7798cbbdd53f989
Warstelide49 <evindrenji01@gmail.com>
2022-08-12 11:04:39 +0530

    stateful comps

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,119 +1,93 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Form from './Form/Form'
 import Preview from './Preview/Preview'
 import Education from './Preview/Education'
 import Experience from './Preview/Experience'
 import '../styles/content.css'
 
+function Content(props){
 
-class Content extends Component{
-	constructor(props){
-		super(props);
-		this.state={
-			main_object:{
-				name:'',
-				email:'',
-				phone:'',
-				address:'',
-				education:[<Education start={''} end={''} title={''} university={''} grade={''} key={0} listId={0}/>],
-				experience:[<Experience start={''} end={''} position={''} company={''} description={''} key={0} listId={0}/>],
-			},
-		}
-		this.changeGeneralInfo=this.changeGeneralInfo.bind(this);
-		this.update=this.update.bind(this);
-		this.add = this.add.bind(this);
-		this.delete = this.delete.bind(this);
+	const [main_object, setMainObject] = useState({
+		name:'',
+		email:'',
+		phone:'',
+		address:'',
+		education:[<Education start={''} end={''} title={''} university={''} grade={''} key={0} listId={0}/>],
+		experience:[<Experience start={''} end={''} position={''} company={''} description={''} key={0} listId={0}/>],
+	});
+
+	const changeGeneralInfo = (name, email, phone, address) => {
+		setMainObject({
+			...main_object,
+			name,
+			email,
+			phone,
+			address,
+		})
 	}
 
-	changeGeneralInfo(name, email, phone, address){
-		this.setState({
-			main_object: {
-				...this.state.main_object,
-				name,
-				email,
-				phone,
-				address,
-			}	
-		});
-	}
-
-	update(object, listId, section){
-		const main = this.state.main_object;
-		this.setState({
-			main_object:{
-				...main,
-				education: main.education.map(item=>{
-					if (section==='education' && item.props.listId===listId){
-						return (<Education start={object.start} end={object.end} title={object.title} university={object.university} grade={object.grade} key={listId} listId={listId}/>)
-					}
-					else{
-						return item
-					}
-				}),
-				experience: main.experience.map(item=>{
-					if (section==='experience' && item.props.listId===listId){
-						return (<Experience start={object.start} end={object.end} position={object.position} company={object.company} description={object.description} key={listId} listId={listId}/>)
-					}
-					else{
-						return item
-					}
-				})
-			}
-		});
-	}
-
-	add(listId, section){
-		const main = this.state.main_object;
-		if(section==='education'){
-			this.setState({
-				main_object:{
-					...main,
-					education:[...main.education, <Education start={''} end={''} title={''} university={''} grade={''} key={listId} listId={listId}/>]
+	const update = (object, listId, section) =>{
+		setMainObject({
+			...main_object,
+			education: main_object.education.map(item=>{
+				if (section==='education' && item.props.listId===listId){
+					return (<Education start={object.start} end={object.end} title={object.title} university={object.university} grade={object.grade} key={listId} listId={listId}/>)
 				}
+				else{
+					return item
+				}
+			}),
+			experience: main_object.experience.map(item=>{
+				if (section==='experience' && item.props.listId===listId){
+					return (<Experience start={object.start} end={object.end} position={object.position} company={object.company} description={object.description} key={listId} listId={listId}/>)
+				}
+				else{
+					return item
+				}
+			})
+		})
+	}
+
+	const add = (listId, section) => {
+		if(section==='education'){
+			setMainObject({
+				...main_object,
+				education:[...main_object.education, <Education start={''} end={''} title={''} university={''} grade={''} key={listId} listId={listId}/>]
 			})
 		}
 		else{
-			this.setState({
-				main_object:{
-					...main,
-					experience:[...main.experience, <Experience start={''} end={''} position={''} company={''} description={''} key={listId} listId={listId}/>]
-				}
+			setMainObject({
+				...main_object,
+				experience:[...main_object.experience, <Experience start={''} end={''} position={''} company={''} description={''} key={listId} listId={listId}/>]
 			})
 		}
 	}
 
-	delete(listId, section){
-		const main = this.state.main_object;
+	const deleteSec = (listId, section) =>{
 		if(section==='education'){
-			this.setState({
-				main_object: {
-					...main,
-					education: main.education.filter((item)=> { 
-		        		return item.props.listId !== listId 
-		    		})
-				}
-		    });
+			setMainObject({
+				...main_object,
+				education: main_object.education.filter((item)=> { 
+	        		return item.props.listId !== listId 
+	    		})
+			})
 		}
 		else{
-			this.setState({
-				main_object: {
-					...main,
-					experience: main.experience.filter((item)=> { 
-		        		return item.props.listId !== listId 
-		    		})
-				}
-		    });
+			setMainObject({
+				...main_object,
+				experience: main_object.experience.filter((item)=> { 
+	        		return item.props.listId !== listId 
+	    		})
+			});
 		}
 	}
 
-	render(){
-		return(
-			<div className='main-container'>
-				<Form changeGeneralInfo={this.changeGeneralInfo} update={this.update} addEducation={this.add} delete={this.delete} addExperience={this.add}/>
-				<Preview main={this.state.main_object}/>
-			</div>
-		);
-	}
+	return(
+		<div className='main-container'>
+			<Form changeGeneralInfo={changeGeneralInfo} update={update} addEducation={add} delete={deleteSec} addExperience={add}/>
+			<Preview main={main_object}/>
+		</div>
+	);
 }
 
 export default Content
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,83 +1,70 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import EduForm from './EducationForm'
 import ExpForm from './ExperienceForm'
 import { v4 as uuidv4 } from 'uuid';
 
-class Form extends Component{
-	constructor(props){
-		super(props);
-		this.state={
-			education:[<EduForm update={this.props.update} deleteEdu={this.props.delete} key={0} listId={0}/>],
-			experience:[<ExpForm update={this.props.update} deleteExp={this.props.delete} key={0} listId={0}/>],
-		}
-		this.updateGeneral=this.updateGeneral.bind(this);
-		this.addBlockEdu=this.addBlockEdu.bind(this);	
-		this.addBlockExp=this.addBlockExp.bind(this);	
-	}
+function Form(props){
 
-	updateGeneral(){
+	const [education, setEducation] = useState([<EduForm update={props.update} deleteEdu={props.delete} key={0} listId={0}/>]);
+	const [experience, setExperience] = useState([<ExpForm update={props.update} deleteExp={props.delete} key={0} listId={0}/>]);
+	
+	const updateGeneral = () => {
 		const name=document.querySelector('#name').value;
 		const email=document.querySelector('#email').value;
 		const phone=document.querySelector('#phone').value;
 		const address=document.querySelector('#address').value;
 		
-		this.props.changeGeneralInfo(name, email, phone, address);
+		props.changeGeneralInfo(name, email, phone, address);
 	}
 
-	addBlockEdu(){
+	const addBlockEdu = () =>{
 		const new_id=uuidv4()
-		this.setState({
-			education:[...this.state.education, <EduForm update={this.props.update} deleteEdu={this.props.delete} key={uuidv4()} listId={new_id}/>]
-		})
-		this.props.addEducation(new_id, 'education')
+		setEducation([...education, <EduForm update={props.update} deleteEdu={props.delete} key={uuidv4()} listId={new_id}/>])
+		props.addEducation(new_id, 'education')
 	}
 
-	addBlockExp(){
+	const addBlockExp = () =>{
 		const new_id=uuidv4()
-		this.setState({
-			experience:[...this.state.experience, <ExpForm update={this.props.update} deleteExp={this.props.delete} key={uuidv4()} listId={new_id}/>]
-		})
-		this.props.addExperience(new_id, 'experience')
+		setExperience([...experience, <ExpForm update={props.update} deleteExp={props.delete} key={uuidv4()} listId={new_id}/>])
+		props.addExperience(new_id, 'experience')
 	}
 
-	render(){
-		return(
-			<div className='form-container'>
-				<h2>Form</h2>
-				<div className='form-general'>
-					<h4>General Information</h4>
-					<form onChange={this.updateGeneral}>
-						<input id='name' placeholder='Name'></input>
-						<input id='email' placeholder='Email'></input>
-						<input id='phone' placeholder='Phone number'></input>
-						<input id='address' placeholder='Address'></input>
-					</form>
+	return(
+		<div className='form-container'>
+			<h2>Form</h2>
+			<div className='form-general'>
+				<h4>General Information</h4>
+				<form onChange={updateGeneral}>
+					<input id='name' placeholder='Name'></input>
+					<input id='email' placeholder='Email'></input>
+					<input id='phone' placeholder='Phone number'></input>
+					<input id='address' placeholder='Address'></input>
+				</form>
+			</div>
+			<div className='form-education'>
+				<div className='spaced-header'>
+					<h4>Education</h4>
+					<button id='add-education' onClick={addBlockEdu}>Add</button>
 				</div>
-				<div className='form-education'>
-					<div className='spaced-header'>
-						<h4>Education</h4>
-						<button id='add-education' onClick={this.addBlockEdu}>Add</button>
-					</div>
-					<div className='form-blocks'>
-						{this.state.education.map((input) => {
-					            return input 
-					    })}
-					</div>
-				</div>
-				<div className='form-experience'>
-					<div className='spaced-header'>
-						<h4>Experience</h4>
-						<button id='add-education' onClick={this.addBlockExp}>Add</button>
-					</div>
-					<div className='form-blocks'>
-						{this.state.experience.map((input) => {
-					            return input 
-					    })}
-					</div>
+				<div className='form-blocks'>
+					{education.map((input) => {
+				            return input 
+				    })}
 				</div>
 			</div>
-		);
-	}
+			<div className='form-experience'>
+				<div className='spaced-header'>
+					<h4>Experience</h4>
+					<button id='add-education' onClick={addBlockExp}>Add</button>
+				</div>
+				<div className='form-blocks'>
+					{experience.map((input) => {
+				            return input 
+				    })}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Form
